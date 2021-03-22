b460c85ad23aa9e199d118a777d406378cd281a2
Arthur Silva <fuscellaarthur@gmail.com>
2021-03-22 13:36:52 -0300

    initialize app with UI5 Web Components

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,35 @@
 import React from 'react';
+import { Avatar, ShellBar, Link } from '@ui5/webcomponents-react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+export default function App() {
   return (
     <div className="App">
+      <ShellBar
+        primaryTitle="Worklist App"
+        notificationCount="99"
+        showNotifications={true}
+        showProductSwitch={true}
+      >
+        <Avatar slot="logo" image={logo} />
+        <Avatar slot="profile" image="https://github.com/afuscella.png" />
+
+      </ShellBar>
+
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
-        <a
+        <Link
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
-          rel="noopener noreferrer"
         >
           Learn React
-        </a>
+        </Link>
       </header>
     </div>
   );
 }
-
-export default App;
